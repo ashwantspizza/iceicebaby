88f48f8c58c022bdc975d151c33ccd51f847eb12
stevenlegere <stevenmarklegere@gmail.com>
2023-08-07 13:02:19 +0100

    adding code for render and card

--- smoothoperator/src/App.js
+++ smoothoperator/src/App.js
@@ -1,12 +1,76 @@
+import { useEffect, useState } from 'react';
+import supabase from './config/supabaseConfig';
+import SmoothieCard from './components/SmoothieCard';
 import './App.css';
 
-function App() {
+const App = () => {
+  console.log(supabase);
+  // State variable to handle smoothie data and allergen filtering
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+  const [selectedAllergens, setSelectedAllergens] = useState([]);
+
+  // Function to handle allergen selection and filtering
+  // const handleAllergensChange = (allergen) => {
+  //   setSelectedAllergens((prev) =>
+  //     prev.includes(allergen) ? prev.filter((item) => item !== allergen) : [...prev, allergen]
+  //   );
+  // };
+
+  // useEffect hook to fetch smoothies from the database on component mount
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from('smoothies').select();
+      // Handle any fetch errors and update state accordingly
+      if (error) {
+        setFetchError('Could not fetch the smoothies');
+        setSmoothies(null);
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+    // Invoke the fetchSmoothies function
+    fetchSmoothies();
+  }, []);
+
+  // Render the App component with smoothie data and allergen filter
   return (
     <div className="App">
-      <h1>Smooth Operator</h1>
-      
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          {/* Add checkboxes for each allergen */}
+          <div className="allergen-filter">
+            {['gluten', 'nuts', 'peanuts', 'milk', 'soy', 'egg', 'sesame'].map((allergen) => {
+              const prefixedAllergen = `no ${allergen}`;
+              return (
+                <label key={allergen}>
+                  <input
+                    type="checkbox"
+                    value={allergen}
+                    checked={selectedAllergens.includes(allergen)}
+                    // onChange={() => handleAllergensChange(allergen)}
+                  />
+                  {prefixedAllergen.charAt(0).toUpperCase() + prefixedAllergen.slice(1)}
+                </label>
+              );
+            })}
+          </div>
+          {/* Filter smoothies based on selected allergens */}
+          <div className="smoothie-grid">
+            {smoothies
+              .filter((smoothie) => !selectedAllergens.some((allergen) => smoothie.allergen.includes(allergen)))
+              .map((smoothie) => (
+                <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+              ))}
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
